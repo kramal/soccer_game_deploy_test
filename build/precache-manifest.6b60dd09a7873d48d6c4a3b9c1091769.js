f51bb68fc9bad3a0d54fec968a77f4ca942c6db7
kramal <kramal888@gmail.com>
2022-09-18 11:46:20 +0200

    d

---
+++ build/precache-manifest.6b60dd09a7873d48d6c4a3b9c1091769.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b68f1a91ff253c3b36123cc9b88fe58",
+    "url": "/soccer_game_deploy_test/index.html"
+  },
+  {
+    "revision": "526adcc1e2490d07d552",
+    "url": "/soccer_game_deploy_test/static/css/2.56af9648.chunk.css"
+  },
+  {
+    "revision": "e910cec739c29e714c4e",
+    "url": "/soccer_game_deploy_test/static/css/main.264d5413.chunk.css"
+  },
+  {
+    "revision": "526adcc1e2490d07d552",
+    "url": "/soccer_game_deploy_test/static/js/2.b3210687.chunk.js"
+  },
+  {
+    "revision": "40603d66a12b4e76df8998e78935ec3a",
+    "url": "/soccer_game_deploy_test/static/js/2.b3210687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e910cec739c29e714c4e",
+    "url": "/soccer_game_deploy_test/static/js/main.35266d20.chunk.js"
+  },
+  {
+    "revision": "d3e3639a086611fb555d",
+    "url": "/soccer_game_deploy_test/static/js/runtime-main.20311ffe.js"
+  },
+  {
+    "revision": "ff039289b46b1a7d82d6bbe6f99ff75c",
+    "url": "/soccer_game_deploy_test/static/media/A1.ff039289.jpg"
+  },
+  {
+    "revision": "861eb5fc988e9d7db6a50710811b2caa",
+    "url": "/soccer_game_deploy_test/static/media/A2.861eb5fc.jpg"
+  },
+  {
+    "revision": "178983fca54e94f238a56743da34f754",
+    "url": "/soccer_game_deploy_test/static/media/image 2.178983fc.png"
+  },
+  {
+    "revision": "2b258356e76bacdfb0bab8bb44f91617",
+    "url": "/soccer_game_deploy_test/static/media/player.2b258356.png"
+  }
+]);
